5c1187fa2accbbfb60932681f5dc2b516b4f1a78
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-07 19:50:57 +0200

    feat: add database connection and update server configuration

---
+++ database/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2')
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'movies'
+})
+
+
+connection.connect((err) => {
+    if(err) {
+        console.error('Error connecting to the database:', err)
+        return
+    }
+    console.log('Connected to the database')
+})
+
+module.exports = connection
