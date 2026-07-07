49535c9ac1e8aa56e9bf8a2a6be59055e9a74ba8
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-07 20:43:37 +0200

    feat: implement show route for movie and reviews

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Ciao ragazzi, è ora di mettere alla prova le vostre conoscenze iniziando a cost
 [x] Creiamo una nuova applicazione Express
 [x] Colleghiamo l’app al db e verifichiamo che tutto funzioni
 [x] Prepariamo una rotta index per ottenere la lista dei film
-[] Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
+[x] Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
 
 **Bonus**
 [] Inserire le immagini nel progetto express
--- server.js
+++ server.js
@@ -17,6 +17,34 @@ app.get('/movies_cover', (req, res) => {
     })
 })
 
+
+app.get('/movies_cover/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    connection.query('SELECT * FROM movies WHERE id = ?', [id], (err, results) => {
+        if(err) {
+            console.error('Error fetching movie:', err)
+            return res.status(500).json({ error: true, message: "Internal Server Error" });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: true, message: "Movie not found" });
+    }
+
+    const movie = results[0]
+    // res.json(movie)
+
+        connection.query('SELECT * FROM reviews WHERE movie_id = ?', [id], (err, reviews) => {
+            if(err) {
+                console.error('Error fetching reviews:', err)
+                return res.status(500).json({ error: true, message: "Internal Server Error" });
+            }
+
+            res.json({ ...movie, reviews })
+        })
+    })
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
